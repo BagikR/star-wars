bac15c70509abf8bef7661e636ac74a9696d7690
BagikR <99125716+BagikR@users.noreply.github.com>
2023-06-14 21:43:52 +0300

    add commit

--- src/services/changeCssVar.js
+++
@@ -1,9 +0,0 @@
-export const changeCssVar = theme => {
-    const root = document.querySelector(':root');
-    const cssVar = ['header', 'bgimage'];
-
-    cssVar.forEach(cssVar => {
-            root.style.setProperty(`--theme-default-${cssVar}`, 
-            `var(--theme-${theme}-${cssVar})`);
-        });
-}
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import favoriteReducer from './favReducers';
-
-export default combineReducers({
-    favoriteReducer
-})
