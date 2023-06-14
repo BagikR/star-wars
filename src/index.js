c4c1d4181b6a7c906bba026b3facb534cb7cdc0d
BagikR <99125716+BagikR@users.noreply.github.com>
2023-06-14 22:16:35 +0300

    add repo

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import store from "@store/store";
 import { Provider } from "react-redux";
 import ThemeProvider from "@context/themeProvider";
+import { REPO_NAME } from '@constants/repo'
 
 import App from "@containers/App";
 import "@styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={`/${REPO_NAME}/`}>
     <Provider store={store}>
       <ThemeProvider>
         <App />
